21ebe194d828b38b932bd106be8e247d4ab34c34
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-12-26 11:39:41 +0300

    add react-router-dom and add pages for client app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { createBrowserRouter,RouterProvider,Route } from "react-router-dom";
+import { Register } from './pages/Register';
+import { Login } from './pages/Login';
+import { Single } from './pages/Single';
+import { Write } from './pages/Write';
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<div>This is home</div>
+  },
+  {
+    path:"/register",
+    element:<Register/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  },
+  {
+    path:"/single",
+    element:<Single/>
+  },
+  {
+    path:"/write",
+    element:<Write/>
+  }
+]);
 function App() {
   return (
     <div>
-      Hello world!
+      <RouterProvider router={router}/>
     </div>
   );
 }
