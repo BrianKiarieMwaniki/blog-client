2d2ef648043504391d3fe37d52988421fc312470
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2022-12-26 18:08:39 +0300

    add all pages and their children components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,62 @@
-import { createBrowserRouter,RouterProvider,Route } from "react-router-dom";
-import { Register } from './pages/Register';
-import { Login } from './pages/Login';
-import { Single } from './pages/Single';
-import { Write } from './pages/Write';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import { Register } from "./pages/Register";
+import { Login } from "./pages/Login";
+import { Single } from "./pages/Single";
+import { Write } from "./pages/Write";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
+import "./style.scss";
+
+const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<div>This is home</div>
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/post/:id",
+        element: <Single />,
+      },
+      {
+        path: "/write",
+        element: <Write />,
+      },
+    ],
   },
   {
-    path:"/register",
-    element:<Register/>
+    path: "/register",
+    element: <Register />,
   },
   {
-    path:"/login",
-    element:<Login/>
+    path: "/login",
+    element: <Login />,
   },
-  {
-    path:"/single",
-    element:<Single/>
-  },
-  {
-    path:"/write",
-    element:<Write/>
-  }
 ]);
 function App() {
   return (
-    <div>
-      <RouterProvider router={router}/>
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
